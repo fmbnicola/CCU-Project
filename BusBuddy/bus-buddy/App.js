b0744fa0695e135956c3c85ed962e59f1797476c
Fmbnicolau <fmbnicolau@hotmail.com>
2019-11-19 15:03:08 +0000

    another module :)
    run "npm update" in folder bus-buddy to get the new modules

--- BusBuddy/bus-buddy/App.js
+++ BusBuddy/bus-buddy/App.js
@@ -1,129 +1,100 @@
 import React from 'react';
-import {Platform, StyleSheet, Text, View, FlatList} from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import Header from './components/Header';
-import InputBar from './components/InputBar';
-import TodoItem from './components/TodoItem';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component{
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation'
 
+import Icon from "react-native-vector-icons/FontAwesome";
 
-  /* Note: in react native the execution loop is the following (Lifecycle Methods):
-  *  
-  *   constructor() -> componentWillMount() -> render() -> componentDidMount()
-  *                                              ^                 |
-  *                                              |_________________|
-  */
-
-  constructor(){
-    super();
-
-    this.state = {
-      todoInput: '', 
-      todos: [
-        { id: 0, title: 'Take out the trash', done: false}, 
-        { id: 1, title: 'Cook dinner', done: false}
-      ]
-    }
-  }
-  
-  //this function is called to add an new element to the list
-  addNewTodo(){
-
-    //make a copy of todos to change by adding new element
-    let todos = this.state.todos;
-    todos.unshift({
-      id: todos.length + 1,
-      title: this.state.todoInput,
-      done: false
-    });
-
-    //update state and clean input buffer
-    this.setState({
-      todos,
-      todoInput: ''
-    })
-  }
-
-  //this function toggles the 'done' property of a todo element
-  toggleDone(item){
-
-    //iterate through array to find the right todo and toggle it
-    let todos = this.state.todos;
-
-    todos = todos.map( (todo) => {
-      if(todo.id == item.id){
-        todo.done = ! todo.done;
-      }
-
-      return todo
-    })
-    
-    //update todos
-    this.setState({todos})
-  }
-
-  //this function removes a todo element
-  removeTodo(item){
-
-    let todos = this.state.todos;
-
-    //remove any todos wit id of the item given (aka delete that todo)
-    todos = todos.filter((todo) => todo.id != item.id);
-    
-    //update todos
-    this.setState({todos})
-  }
-
-  render(){
-    // status bar - only needed for ios
-    const statusbar = (Platform.OS == 'android')? <View style={styles.statusbar}></View> : <View></View>
-
+export default class App extends React.Component {
+  render() {
     return (
-      <View style={styles.container}>
+        <AppContainer />
+    );
+  }
+}
 
-        {statusbar}
-        
-        <Header title = "Bus Buddy" ></Header>
-
-        <InputBar 
-          /* here we create a function as a prop to change the text in the state*/
-          changeTextFunc = {(todoInput) => this.setState({todoInput})}
-          addNewTodo     = { () => this.addNewTodo()}
-          todoInput      = {this.state.todoInput}
-        ></InputBar>
-
-        <FlatList 
-          //Flatlist updates data when things are added to extraData 
-          //(in this case when the state is changed)
-          data         = {this.state.todos}
-          extraData    = {this.state}
-
-          keyExtractor = { (item, index) => index.toString()}
-          renderItem   = { ({item, index}) => {
-            return (
-              <TodoItem 
-                todoItem={item} 
-                toggleDone = {() => this.toggleDone(item)}
-                removeTodo ={ () => this.removeTodo(item)}
-              />
-            )
-          }}
-        />
+class HomeScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> This is my Home screen </Text>
       </View>
     );
   }
-
 }
 
-//creates the style sheet 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  statusbar: {
-    backgroundColor: '#0055aa',
-    height: 24
+class ExploreScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#d0d0d0'}}>
+        <Text> This is my Explore screen </Text>
+      </View>
+    );
   }
-});
+}
+
+class NotificationsScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff'}}>
+        <Text> This is my Notifications screen </Text>
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#d0d0d0'}}>
+        <Text> This is my Profile screen </Text>
+      </View>
+    );
+  }
+}
+
+const bottomTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="home" size={25} color={tintColor} />
+        )
+      }
+    },
+    Explore: {
+      screen: ExploreScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="comments" size={25} color={tintColor} />
+        )
+      }
+    },
+    Notifications: {
+      screen: NotificationsScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="search" size={25} color={tintColor} />
+        )
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="user" size={25} color={tintColor} />
+        )
+      }
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    tabBarOptions: {
+      activeTintColor: '#eb6e3d'
+    }
+  }
+);
+
+const AppContainer = createAppContainer(bottomTabNavigator);
