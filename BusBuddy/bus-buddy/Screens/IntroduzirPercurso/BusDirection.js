32256b47fdc5ae2b6d2a1e8d08f229aab2f359a9
Alexandra Maroco <alexandra.maroco@tecnico.ulisboa.pt>
2019-11-24 12:52:35 +0000

    Bus Direction Design %

--- BusBuddy/bus-buddy/Screens/IntroduzirPercurso/BusDirection.js
+++ BusBuddy/bus-buddy/Screens/IntroduzirPercurso/BusDirection.js
@@ -37,14 +37,14 @@ export default class BusDirection extends React.Component {
         )
     }
     return(
-        <View style = {{flexDirection:'column', alignItems:'center',justifyContent:'center', position:'relative', top:'15%',padding:5}}>
+        <View style = {{flexDirection:'column', alignItems:'center',justifyContent:'space-between', position:'relative', top:'15%'}}>
           <TouchableOpacity onPress = {() => {navigate('InputBus', {})}} style = {styles.backButton}>
                 <View style = {{flexDirection:'row',justifyContent:'space-around', alignItems:'center'}}>
                   <Image style = {styles.backImage} source={require('./back.png')} />
                   <Text style = {styles.backText}>BACK</Text>
                 </View>
           </TouchableOpacity>
-          <Text style = {styles.Text}>Escolha o sentido do autocarro </Text><Text style = {styles.Text}>{this.state.numBus}</Text>
+          <Text style = {styles.Text}>Escolha o sentido do autocarro </Text><Text style = {styles.numBusText}>{this.state.numBus}</Text>
 
           <TouchableOpacity onPress = {() => {navigate('StopsList', {
                 busNumber: this.state.numBus, 
@@ -73,7 +73,7 @@ export default class BusDirection extends React.Component {
 }
 
   const styles = StyleSheet.create({
-   button: {
+    /*button: {
         display: 'flex',
         height: 50,
         width: 50,
@@ -82,8 +82,6 @@ export default class BusDirection extends React.Component {
         alignItems: 'center',
         backgroundColor: 'white',
         position: 'relative',
-
-
       },
     backButton: {
          display: 'flex',
@@ -99,7 +97,34 @@ export default class BusDirection extends React.Component {
      backImage: {
          height: 20,
          width: 20,
+     }, */
+     button: {
+      display: 'flex',
+      height: '20%',
+      width: '20%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'transparent',
+      position: 'relative',
+      top: '30%',
+      //position: 'relative',
+      //top: '15%',
+      //left: '25%',
+    },
+  backButton: {
+       display: 'flex',
+       height: '10%',
+       justifyContent: 'center',
+       alignSelf: 'flex-start',
+       backgroundColor: 'white',
+       width: '20%',
+       position: 'relative',
+       left: '3%',
      },
+   backImage: {
+       height: '100%',
+       width: '20%',
+   },
     image: {
         display: 'flex',
         height: 50,
@@ -107,7 +132,7 @@ export default class BusDirection extends React.Component {
         justifyContent: 'center',
         alignItems: 'center',
     },
-    Text: {
+    /*Text: {
        fontSize: 28,
        width:350,
        //textTransform: 'uppercase',
@@ -124,7 +149,41 @@ export default class BusDirection extends React.Component {
       textAlign:'center',
       position: 'relative',
       right: 3,
+  },*/
+  Text: {
+    fontSize: 25,
+    width:'80%',
+    //textTransform: 'uppercase',
+    color: 'grey',
+    textAlign:'center',
+    //position: 'relative',
+    //top: '0%',
+    //left: '8%',
   },
+  numBusText: {
+    fontSize: 25,
+    width:'40%', //small
+    width: '80%', //same size as destination buttons
+    //textTransform: 'uppercase',
+    color: 'grey',
+    textAlign:'center',
+    borderRadius:10,
+    borderBottomWidth: 0.5,
+    borderBottomColor:'grey',
+    borderTopWidth: 0.5,
+    borderTopColor:'grey',
+    borderLeftWidth: 0.5,
+    borderLeftColor:'grey',
+    borderRightWidth: 0.5,
+    borderRightColor:'grey',
+  },
+  backText: {
+    fontSize: 16,
+    width:'50%',
+    //textTransform: 'uppercase',
+    color: 'grey',
+    textAlign:'center',      
+},
    keyBoard: {
       fontSize: 20,
       width:350,
@@ -136,19 +195,45 @@ export default class BusDirection extends React.Component {
   },
   destText: {
      fontSize: 20,
-     width:190,
+     //width:'100%',
      //textTransform: 'uppercase',
-     color: '#ffffff',
+     //color: '#5A6978',
+     color:'white',
      textAlign:'center',
  },
  direcao1: {
      display: 'flex',
-     margin:30,
-     height: 70,
-     width:190,
+     //margin: '30%',
+     height: '20%',
+     width: '80%',
      justifyContent: 'center',
      alignItems: 'center',
-     top:27,
+     alignSelf: 'center',
+     //top: '30%',
+
+     borderRadius:20,
+     borderBottomWidth: 0.5,
+     borderBottomColor:'grey',
+     borderTopWidth: 0.5,
+     borderTopColor:'grey',
+     borderLeftWidth: 0.5,
+     borderLeftColor:'grey',
+     borderRightWidth: 0.5,
+     borderRightColor:'grey',
+     backgroundColor: '#47525E',
+     //shadowColor: '#47525E',
+     shadowColor: 'white',
+     shadowOpacity: 0.4,
+     shadowOffset: { height: 10, width: 0 },
+     shadowRadius: 20,
+ },
+ direcao2: {
+     /*display: 'flex',
+     height: 70,
+     width:190,
+     margin:30,
+     justifyContent: 'center',
+     alignItems: 'center',
 
      borderRadius:20,
      borderBottomWidth: 0.5,
@@ -163,15 +248,16 @@ export default class BusDirection extends React.Component {
      shadowColor: '#2AC062',
      shadowOpacity: 0.4,
      shadowOffset: { height: 10, width: 0 },
-     shadowRadius: 20,
- },
- direcao2: {
+     shadowRadius: 20,*/
+
      display: 'flex',
-     height: 70,
-     width:190,
-     margin:30,
+     //margin: '30%',
+     height: '20%',
+     width: '80%',
      justifyContent: 'center',
      alignItems: 'center',
+     alignSelf: 'center',
+     //top: '30%',
 
      borderRadius:20,
      borderBottomWidth: 0.5,
@@ -182,8 +268,9 @@ export default class BusDirection extends React.Component {
      borderLeftColor:'grey',
      borderRightWidth: 0.5,
      borderRightColor:'grey',
-     backgroundColor: '#0066cc',
-     shadowColor: '#2AC062',
+     backgroundColor: '#47525E',
+     //shadowColor: '#47525E',
+     shadowColor: 'white',
      shadowOpacity: 0.4,
      shadowOffset: { height: 10, width: 0 },
      shadowRadius: 20,
