55fd6b8a5422b67a24a7dc2dbcf7b39005bee8fe
Alexandra Maroco <alexandra.maroco@tecnico.ulisboa.pt>
2019-11-24 15:15:20 +0000

    ReadyToGo Design %

--- BusBuddy/bus-buddy/Screens/IntroduzirPercurso/ReadyToGo.js
+++ BusBuddy/bus-buddy/Screens/IntroduzirPercurso/ReadyToGo.js
@@ -23,7 +23,7 @@ export default class ReadyToGo extends React.Component {
     const {navigate} = this.props.navigation;
 
     return(
-        <View style = {{flexDirection:'column', alignItems:'center',justifyContent:'center', position:'relative', top:'15%',padding:5}}>
+        <View style = {{flexDirection:'column', alignItems:'center',justifyContent:'space-between', position:'relative', top:'15%'}}>
 
           <TouchableOpacity onPress = {() => {navigate('StopsList', {})}} style = {styles.backButton}>
                 <View style = {{flexDirection:'row',justifyContent:'space-around', alignItems:'center'}}>
@@ -35,7 +35,7 @@ export default class ReadyToGo extends React.Component {
           <Text style = {styles.Text}>Paragem selecionada:</Text>
           <Text style = {styles.SmallText}>{this.state.ini_stop.name}</Text>
           
-          <Text> {"\n\n\n\n\n\n"} </Text>
+          <Text> {"\n\n"} </Text>
 
           <Text style = {styles.Text}>Destino selecionado:</Text>
           <Text style = {styles.SmallText}>{this.state.fin_stop.name}</Text>
@@ -52,7 +52,7 @@ export default class ReadyToGo extends React.Component {
 }
 
   const styles = StyleSheet.create({
-   button: {
+   /*button: {
         display: 'flex',
         height: 50,
         width: 50,
@@ -76,7 +76,34 @@ export default class ReadyToGo extends React.Component {
      backImage: {
          height: 20,
          width: 20,
-     },
+     },*/
+     button: {
+      display: 'flex',
+      height: '20%',
+      width: '20%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'transparent',
+      position: 'relative',
+      top: '30%',
+      //position: 'relative',
+      //top: '15%',
+      //left: '25%',
+    },
+      backButton: {
+        display: 'flex',
+        height: '10%',
+        justifyContent: 'center',
+        alignSelf: 'flex-start',
+        backgroundColor: 'white',
+        width: '20%',
+        position: 'relative',
+        left: '3%',
+      },
+    backImage: {
+        height: '100%',
+        width: '20%',
+    },
     image: {
         display: 'flex',
         height: 50,
@@ -85,31 +112,60 @@ export default class ReadyToGo extends React.Component {
         alignItems: 'center',
     },
     Text: {
-       fontSize: 18,
+       /* fontSize: 18,
        width:350,
        //textTransform: 'uppercase',
        color: 'grey',
        textAlign:'center',
        position: 'relative',
-       top: 45,
+       top: 45, */
+      fontSize: 25,
+      width:'80%',
+      //textTransform: 'uppercase',
+      color: 'grey',
+      textAlign:'center',
+      //position: 'relative',
+      //top: '0%',
+      //left: '8%',
    },
     SmallText: {
-      fontSize: 16,
+      /* fontSize: 16,
       width:300,
       //textTransform: 'uppercase',
       color: 'black',
       textAlign:'center',
       position: 'relative',
-      top: 45,
+      top: 45, */
+      fontSize: 25,
+      width:'85%',
+      //textTransform: 'uppercase',
+      color: 'grey',
+      textAlign:'center',
+      top: '5%',
+
+      borderRadius:10,
+      borderBottomWidth: 0.5,
+      borderBottomColor:'grey',
+      borderTopWidth: 0.5,
+      borderTopColor:'grey',
+      borderLeftWidth: 0.5,
+      borderLeftColor:'grey',
+      borderRightWidth: 0.5,
+      borderRightColor:'grey',
     },
     backText: {
-        fontSize: 16,
+        /*fontSize: 16,
         width:100,
         //textTransform: 'uppercase',
         color: 'grey',
         textAlign:'center',
         position: 'relative',
-        right: 3,
+        right: 3, */
+        fontSize: 16,
+        width:'50%',
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
     },
     keyBoard: {
         fontSize: 20,
@@ -121,14 +177,21 @@ export default class ReadyToGo extends React.Component {
         top: 60,
     },
     destText: {
-      fontSize: 20,
+      /* fontSize: 20,
       width:190,
       //textTransform: 'uppercase',
       color: '#ffffff',
+      textAlign:'center', */
+
+      fontSize: 20,
+      //width:'100%',
+      //textTransform: 'uppercase',
+      //color: '#5A6978',
+      color:'white',
       textAlign:'center',
     },
     letsgo: {
-      display: 'flex',
+      /* display: 'flex',
       margin:30,
       height: 70,
       width:210,
@@ -147,6 +210,31 @@ export default class ReadyToGo extends React.Component {
       borderRightColor:'grey',
       backgroundColor: '#0066cc',
       shadowColor: '#2AC062',
+      shadowOpacity: 0.4,
+      shadowOffset: { height: 10, width: 0 },
+      shadowRadius: 20, */
+
+      display: 'flex',
+      //margin: '30%',
+      height: '20%',
+      width: '80%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignSelf: 'center',
+      top: '20%',
+
+      borderRadius:20,
+      borderBottomWidth: 0.5,
+      borderBottomColor:'grey',
+      borderTopWidth: 0.5,
+      borderTopColor:'grey',
+      borderLeftWidth: 0.5,
+      borderLeftColor:'grey',
+      borderRightWidth: 0.5,
+      borderRightColor:'grey',
+      backgroundColor: '#47525E',
+      //shadowColor: '#47525E',
+      shadowColor: 'white',
       shadowOpacity: 0.4,
       shadowOffset: { height: 10, width: 0 },
       shadowRadius: 20,
