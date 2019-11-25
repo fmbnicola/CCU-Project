d3c9b633bb87f34f4b605cb0c55ba0ff01bb6e38
cmsa6 <cristianasilveiraantunes@tecnico.ulisboa.pt>
2019-11-25 01:17:22 +0000

    backs adicionaddos. O do busestimation nao esta a funcionaar bem

--- BusBuddy/bus-buddy/Screens/ParagensProximas/BusEstimationScreen.js
+++ BusBuddy/bus-buddy/Screens/ParagensProximas/BusEstimationScreen.js
@@ -35,24 +35,129 @@ export default class BusEstimationScreen extends React.Component {
 
   //BEWARE: the view bellow has clicks disabled.. for now :)
   render() {
+    const {navigate} = this.props.navigation;
+
     return(
       <View pointerEvents="none" style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style = {styles.Text}>{this.state.stop}</Text>
-        <BusList bus_stop_id={this.state.id} num_results='10' updateSelected = {this.updateSelected}/>
+      <TouchableOpacity onPress = {() => {navigate('ParagensProx', {})}} style = {styles.backButton}>
+            <View style = {{flexDirection:'row',justifyContent:'space-around', alignItems:'center'}}>
+              <Image style = {styles.backImage} source={require('./back.png')} />
+              <Text style = {styles.backText}>BACK</Text>
+            </View>
+      </TouchableOpacity>
+          <Text style = {styles.Text}>{this.state.stop}</Text>
+          <BusList bus_stop_id={this.state.id} num_results='10' updateSelected = {this.updateSelected}/>
       </View>
     );
   }
 }
+
+
 const styles = StyleSheet.create({
-  Text: {
-     fontSize: 18,
-     width:350,
-     height:25,
-     //textTransform: 'uppercase',
-     color: 'grey',
-     textAlign:'center',
-     margin:30,
- },
+    button: {
+        display: 'flex',
+        height: '30%',
+        width: '30%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        backgroundColor: 'transparent',
+        position: 'relative',
+        top:'30%',
 
+    },
+    /*backButton: {
+        display: 'flex',
+        height: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        width: 70,
+        position: 'relative',
+        left: 15,
+        },
+    backImage: {
+        height: 20,
+        width: 20,
+    },*/
+    backButton: {
+        display: 'flex',
+        height: '10%',
+        justifyContent: 'center',
+        alignSelf: 'flex-start',
+        backgroundColor: 'white',
+        width: '20%',
+        position: 'relative',
+        left: '3%',
+      },
+    backImage: {
+        height: '100%',
+        width: '20%',
+    },
+    image: {
+        display: 'flex',
+        height: 50,
+        width: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    Text: {
+        fontSize: 28,
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+        /* position: 'relative',
+        top: 45,*/
+    },
+    TextSmall: {
+        fontSize: 25,
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+        /* position: 'relative',
+        top: 45, */
+        width: '85%',
+        alignSelf: 'center',
+        borderRadius:20,
+        borderBottomWidth: 0.5,
+        borderBottomColor:'grey',
+        borderTopWidth: 0.5,
+        borderTopColor:'grey',
+        borderLeftWidth: 0.5,
+        borderLeftColor:'grey',
+        borderRightWidth: 0.5,
+        borderRightColor:'grey',
+    },
+    /*backText: {
+        fontSize: 16,
+        width:100,
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+        position: 'relative',
+        right: 3,
+    },*/
+    backText: {
+        fontSize: 16,
+        width:'50%',
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+    },
+        keyBoard: {
+        fontSize: 20,
+        width:350,
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+        position: 'relative',
+        top: 60,
+    },
 
+    loader:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff"
+    }
 });
--- BusBuddy/bus-buddy/Screens/ParagensProximas/NearestStopsScreen.js
+++ BusBuddy/bus-buddy/Screens/ParagensProximas/NearestStopsScreen.js
@@ -1,8 +1,6 @@
 
 import React from 'react';
-import {
-  View,
-  Text} from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, Image, TextInput } from 'react-native';
 
 import BusStopList from './../API_components/BusStopList';
 
@@ -38,10 +36,127 @@ export default class NearestStopsScreen extends React.Component {
   render() {
     console.log("this.updateSelected");
     console.log(this.updateSelected);
+    const {navigate} = this.props.navigation;
+
     return(
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'}}>
+        <TouchableOpacity onPress = {() => {navigate('Home', {})}} style = {styles.backButton}>
+              <View style = {{flexDirection:'row',justifyContent:'space-around', alignItems:'center'}}>
+                <Image style = {styles.backImage} source={require('./back.png')} />
+                <Text style = {styles.backText}>BACK</Text>
+              </View>
+        </TouchableOpacity>
         <BusStopList  target = 'nearest' updateSelected = {this.updateSelected}/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    button: {
+        display: 'flex',
+        height: '30%',
+        width: '30%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        backgroundColor: 'transparent',
+        position: 'relative',
+        top:'30%',
+
+    },
+    /*backButton: {
+        display: 'flex',
+        height: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        width: 70,
+        position: 'relative',
+        left: 15,
+        },
+    backImage: {
+        height: 20,
+        width: 20,
+    },*/
+    backButton: {
+        display: 'flex',
+        height: '10%',
+        justifyContent: 'center',
+        alignSelf: 'flex-start',
+        backgroundColor: 'white',
+        width: '20%',
+        position: 'relative',
+        left: '3%',
+      },
+    backImage: {
+        height: '100%',
+        width: '20%',
+    },
+    image: {
+        display: 'flex',
+        height: 50,
+        width: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    Text: {
+        fontSize: 28,
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+        /* position: 'relative',
+        top: 45,*/
+    },
+    TextSmall: {
+        fontSize: 25,
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+        /* position: 'relative',
+        top: 45, */
+        width: '85%',
+        alignSelf: 'center',
+        borderRadius:20,
+        borderBottomWidth: 0.5,
+        borderBottomColor:'grey',
+        borderTopWidth: 0.5,
+        borderTopColor:'grey',
+        borderLeftWidth: 0.5,
+        borderLeftColor:'grey',
+        borderRightWidth: 0.5,
+        borderRightColor:'grey',
+    },
+    /*backText: {
+        fontSize: 16,
+        width:100,
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+        position: 'relative',
+        right: 3,
+    },*/
+    backText: {
+        fontSize: 16,
+        width:'50%',
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+    },
+        keyBoard: {
+        fontSize: 20,
+        width:350,
+        //textTransform: 'uppercase',
+        color: 'grey',
+        textAlign:'center',
+        position: 'relative',
+        top: 60,
+    },
+
+    loader:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff"
+    }
+});
