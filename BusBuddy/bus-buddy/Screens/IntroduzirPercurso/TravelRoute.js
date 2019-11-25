507732304701952ff316d21ddb939e3c61b3d0d7
Fmbnicolau <fmbnicolau@hotmail.com>
2019-11-25 00:53:13 +0000

    travel route mk2

--- BusBuddy/bus-buddy/Screens/IntroduzirPercurso/TravelRoute.js
+++ BusBuddy/bus-buddy/Screens/IntroduzirPercurso/TravelRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, Image, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, Image, ActivityIndicator, Modal } from 'react-native';
 
 
 export default class TravelRoute extends React.Component {
@@ -14,7 +14,14 @@ export default class TravelRoute extends React.Component {
             passedStops: [],
             upcomingStop_i: 0,
             upcomingStop: params.allStops[0],
+            modalVisible: false,
+            announceStop: false
         };
+    }
+
+
+    toggleModal(visible) {
+        this.setState({ modalVisible: visible });
     }
 
 
@@ -27,53 +34,98 @@ export default class TravelRoute extends React.Component {
         //increment upcoming stop index
         let new_upcomingStop_i = this.state.upcomingStop_i + 1;
 
-        //if the upcoming stop is the last then we are done, press Stop
-        if(new_upcomingStop_i == this.state.allStops.length-1){
-            this.pressStop();
-        }
         //otherwise, get new upcoming stop
         let new_upcomingStop = this.state.allStops[new_upcomingStop_i];
-     
 
         //update state and clean input buffer
         this.setState({
             passedStops:    new_passedStops,
             upcomingStop_i: new_upcomingStop_i,
-            upcomingStop:   new_upcomingStop
+            upcomingStop:   new_upcomingStop,
+            announceStop:   true
         })
     }
 
+    componentDidUpdate(){
+      
+        if(this.state.upcomingStop_i == this.state.allStops.length-1){
+            this.pressStop();
+        }
+        else if(this.state.announceStop){
+            this.announceStop();
+
+            this.setState({
+                announceStop: false
+            });
+        }
+    }
     
     pressStop(){
         const {navigate} = this.props.navigation;
 
-        lastStopName = this.state.upcomingStop.name;
+        var lastStopName = this.state.upcomingStop.name;
         Alert.alert(
-            'Próxima Paragem: ' + {lastStopName},
+            'Ultima Paragem: ' + lastStopName,
             'Pressione o botão de Stop do autocarro para terminar a sua viagem.',
             [
-              {text: 'OK', onPress: () => {navigate('Home')}},
+              {text: 'Terminar', onPress: () => {navigate('Home')}},
             ],
             {cancelable: false},
         );
     }
 
+
+    announceStop(){
+
+        var stopName = this.state.upcomingStop.name;
+        Alert.alert(
+            'Próxima Paragem: ' + stopName,
+            '',
+            [
+              {text: 'OK', onPress: () => {}},
+            ],
+            {cancelable: false},
+        );
+    }
+
+
     render(){
         const {navigate} = this.props.navigation;
 
-
-        const upcomingStop = <View style = {styles.upcomingStop}>
-                                <Text> {this.state.upcomingStop.name} </Text> 
-                            </View>
         return(
-            <View>
-                {upcomingStop}
-                <Button title='Next' onPress={() => {this.passByBusStop()}}></Button>
+            <View style = {{flexDirection:'column', alignItems:'center',justifyContent:'center', position:'relative', top:'15%',padding:5}}>
+                
+                <Modal animationType = {"slide"} transparent = {false}
+                    visible = {this.state.modalVisible}
+                    onRequestClose = {() => { console.log("Modal has been closed.") } }>
 
+                    <View style = {styles.modal}>
 
-                <TouchableOpacity onPress={() => {navigate('Home')}}>
-                    <Text>Terminar Viagem</Text>
+                        <TouchableOpacity onPress = {() => {this.toggleModal(!this.state.modalVisible)}}>
+                            <Text>Close Modal</Text>
+                        </TouchableOpacity>
+
+                    </View>
+                </Modal>
+
+                <Text style= {styles.Text}> Aproxima-se da Paragem:</Text>
+
+                <View style = {styles.upcomingStop}>
+                    <Text style= {styles.Text2}> {this.state.upcomingStop.name} </Text> 
+                </View>
+
+                <TouchableOpacity style= {styles.endTrip} onPress={() => {navigate('Home')}}>
+                    <Text style= {styles.endText}>Terminar Viagem</Text>
                 </TouchableOpacity>
+
+                <TouchableOpacity style= {styles.passed} onPress={() => {this.toggleModal(!this.state.modalVisible)}}>
+                    <Text style= {styles.endText}>Paragens Anteriores</Text>
+                </TouchableOpacity>
+                
+                <View style={styles.next}>
+                    <Text> (This button is here to simulate the bus moving)</Text>
+                    <Button title='Next' onPress={() => {this.passByBusStop()}}></Button>
+                </View>
             </View>
         )
     }
@@ -82,6 +134,82 @@ export default class TravelRoute extends React.Component {
 
 const styles = StyleSheet.create({
     upcomingStop:{
-        top: 100,
+    },
+    next:{
+        position: 'relative',
+        top: 200
+    },
+    Text: {
+        fontSize: 28,
+        color: '#999',
+        textAlign:'center',
+    },
+    Text2: {
+        fontSize: 22,
+        color: '#555',
+        textAlign:'center',
+        
+    },
+    endTrip: {
+        display: 'flex',
+        top: '50%',
+        height: '15%',
+        width: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        //top: '30%',
+   
+        borderRadius:20,
+        borderBottomWidth: 0.5,
+        borderBottomColor:'grey',
+        borderTopWidth: 0.5,
+        borderTopColor:'grey',
+        borderLeftWidth: 0.5,
+        borderLeftColor:'grey',
+        borderRightWidth: 0.5,
+        borderRightColor:'grey',
+        backgroundColor: '#47525E',
+        //shadowColor: '#47525E',
+        shadowColor: 'white',
+        shadowOpacity: 0.4,
+        shadowOffset: { height: 10, width: 0 },
+        shadowRadius: 20,
+    },
+    passed:{
+        display: 'flex',
+        height: '15%',
+        width: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        //top: '30%',
+   
+        borderRadius:20,
+        borderBottomWidth: 0.5,
+        borderBottomColor:'grey',
+        borderTopWidth: 0.5,
+        borderTopColor:'grey',
+        borderLeftWidth: 0.5,
+        borderLeftColor:'grey',
+        borderRightWidth: 0.5,
+        borderRightColor:'grey',
+        backgroundColor: '#47525E',
+        //shadowColor: '#47525E',
+        shadowColor: 'white',
+        shadowOpacity: 0.4,
+        shadowOffset: { height: 10, width: 0 },
+        shadowRadius: 20,
+    },
+    endText: {
+        fontSize: 20,
+        color:'white',
+        textAlign:'center',
+    },
+    modal: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 100
     },
 });
